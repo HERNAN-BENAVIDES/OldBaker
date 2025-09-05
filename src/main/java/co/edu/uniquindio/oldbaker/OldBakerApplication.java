f564df95d0bd0e92412ba10a0e8af48236a9e09f
HERNAN-BENAVIDES <hernand.benavidesc@uqvirtual.edu.co>
2025-09-05 14:58:09 -0500

    Habilita el envío asíncrono de correos electrónicos en el servicio de correo

--- src/main/java/co/edu/uniquindio/oldbaker/OldBakerApplication.java
+++ src/main/java/co/edu/uniquindio/oldbaker/OldBakerApplication.java
@@ -1,9 +1,10 @@
 package co.edu.uniquindio.oldbaker;
 
-import co.edu.uniquindio.oldbaker.services.MailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class OldBakerApplication {
 	public static void main(String[] args) {
--- src/main/java/co/edu/uniquindio/oldbaker/services/MailService.java
+++ src/main/java/co/edu/uniquindio/oldbaker/services/MailService.java
@@ -4,6 +4,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -26,6 +27,7 @@ public class MailService {
     }
 
 
+    @Async
     public void sendEmail(String to, String subject, Map<String, Object> variables) throws MessagingException {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
