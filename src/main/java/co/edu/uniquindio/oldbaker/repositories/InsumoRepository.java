90ff2edcab32346a0a958f012bce3779a105677f
HERNAN-BENAVIDES <hernand.benavidesc@uqvirtual.edu.co>
2025-10-11 20:33:18 -0500

    Agrega repositorios para Insumo, Producto y Receta

---
+++ src/main/java/co/edu/uniquindio/oldbaker/repositories/InsumoRepository.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.oldbaker.repositories;
+
+import co.edu.uniquindio.oldbaker.model.Insumo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InsumoRepository extends JpaRepository<Insumo, Long> {
+}
+
